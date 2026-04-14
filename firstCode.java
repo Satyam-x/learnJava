6eccebe7f3fc6be847a824ed4774d9443ef23c62
Satyam <satyamporwal123456@gmail.com>
2026-04-15 00:13:25 +0530

    Second commit

--- firstCode.java
+++ firstCode.java
@@ -1,1 +1,24 @@
-First code of java
+// First code of java
+/* 
+// First way
+
+public class firstCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+*/
+
+// Second way
+
+class secondWay {
+    void code01(){
+    System.out.println("Hello world, I am your first choise.");
+    }
+}
+public class firstCode {
+    public static void main(String[] args) {
+        secondWay obj=new secondWay();
+        obj.code01();
+    }
+}
